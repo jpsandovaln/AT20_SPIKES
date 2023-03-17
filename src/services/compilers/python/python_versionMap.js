0d288742829bd099b52fa5547e78ab557324ce84
Daniel Carrasco <Daniel.Carrasco@jala.university>
2023-03-16 22:15:05 -0400

    Add all the routes in the new folder of routes

--- src/services/compilers/python/python_versionMap.js
+++ src/services/compilers/python/python_versionMap.js
@@ -1,4 +1,8 @@
 const VERSIONES = {
-    '3.7': 'D:/jala_files/progra101/pyhton_versiones/python-3.7.0-embed-amd64/python.exe',
+    '3.7': 'D:/jala_files/progra101/at20/AT20_COMPILER_SERVICE/thirdparties/pytthon_versions/python-3.7.0-embed-amd64/python.exe',
+    '3.8': 'D:/jala_files/progra101/at20/AT20_COMPILER_SERVICE/thirdparties/pytthon_versions/python-3.8.0-embed-amd64/python.exe',
+    '3.9': 'D:/jala_files/progra101/at20/AT20_COMPILER_SERVICE/thirdparties/pytthon_versions/python-3.9.0-embed-amd64/python.exe',
+    '3.10': 'D:/jala_files/progra101/at20/AT20_COMPILER_SERVICE/thirdparties/pytthon_versions/python-3.10.0-embed-amd64/python.exe',
+    '3.11': 'D:/jala_files/progra101/at20/AT20_COMPILER_SERVICE/thirdparties/pytthon_versions/python-3.11.0-embed-amd64/python.exe',
 }
 module.exports = VERSIONES;

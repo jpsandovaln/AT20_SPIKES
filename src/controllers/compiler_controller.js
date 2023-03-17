030304736f83dab471d9520b9b9ad5280513fda7
Daniel Carrasco <Daniel.Carrasco@jala.university>
2023-03-16 22:07:51 -0400

    Add some version parameters to controller file

--- src/controllers/compiler_controller.js
+++ src/controllers/compiler_controller.js
@@ -6,9 +6,10 @@ class CompilerController{
         const service = new CompilationService();
 
         const { language } = req.body;
+        const { version } = req.body;
         const  saved_file_path = `${req.file.destination}/${req.file.filename}`;
 
-        const response = await service.run(saved_file_path, language)
+        const response = await service.run(saved_file_path, language, version)
                                     .catch(err => ({'error': err.message}));
 
         res.status(200).send(response);
